28d228dfd515a237afe6b4f53131672869ad2dfd
ike a <iyke.arum@gmail.com>
2018-03-10 19:39:20 -0800

    Final Commit

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+
+
+        private readonly CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            this.context = dbContext;
+        }
+
+
+
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IList<Menu> cheesemenu = context.Menu.ToList();
+
+            return View(cheesemenu);
+        }
+
+
+        public IActionResult Add()
+        {
+            AddMenuViewModel addMenuViewModel = new AddMenuViewModel();
+            return View(addMenuViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddMenuViewModel addMenuViewModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Menu newMenu = new Menu()
+                {
+                    Name = addMenuViewModel.Name
+                };
+
+                context.Menu.Add(newMenu);
+                context.SaveChanges();
+                return Redirect("/Menu/ViewMenu/" + newMenu.ID);
+            }
+
+            return View(addMenuViewModel);
+        }
+
+
+
+        public IActionResult ViewMenu(int id)
+        {
+
+            List<CheeseMenu> items = context.CheesesMenu.Include(item => item.Cheese).Where(cm => cm.MenuID == id).ToList();
+
+
+            Menu aMenu = context.Menu.Single(m => m.ID == id);
+
+            ViewMenuViewModel viewMenuViewModel = new ViewMenuViewModel()
+            {
+                Menu = aMenu,
+                Items = items
+
+
+            };
+
+            return View(viewMenuViewModel);
+        }
+
+
+
+
+        public IActionResult AddItem(int id)
+        {
+            Menu aMenu = context.Menu.Single(m => m.ID == id);
+            List<Cheese> cheeses = context.Cheeses.ToList();
+            AddMenuItemViewModel viewModel = new AddMenuItemViewModel(aMenu, cheeses);
+            return View(viewModel);
+
+        }
+        
+        [HttpPost]
+        public IActionResult AddItem(AddMenuItemViewModel addMenuItemViewModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var cheeseId = addMenuItemViewModel.CheeseID;
+                var menuId = addMenuItemViewModel.MenuID;
+                IList<CheeseMenu> existingItems = context.CheesesMenu.Where(cm => cm.CheeseID == cheeseId).Where(cm => cm.MenuID == menuId).ToList();
+
+                if (existingItems.Count == 0)
+                {
+                    CheeseMenu menuItem = new CheeseMenu
+                    {
+                        CheeseID = cheeseId,
+                        MenuID = menuId
+                    };
+
+                    context.CheesesMenu.Add(menuItem);
+                    context.SaveChanges();
+                    return Redirect(string.Format("/Menu/ViewMenu/{0}", addMenuItemViewModel.MenuID));
+                }
+
+            }
+                return View(addMenuItemViewModel);
+
+        }
+
+    }
+}
---
+++ src/CheeseMVC/Migrations/20180310194033_AddMenu.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CheeseMVC.Migrations
+{
+    public partial class AddMenu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menu",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menu", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cheeses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoryID = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cheeses", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Cheeses_Categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CheesesMenu",
+                columns: table => new
+                {
+                    CheeseID = table.Column<int>(nullable: false),
+                    MenuID = table.Column<int>(nullable: false),
+                    CheeseMenuCheeseID = table.Column<int>(nullable: true),
+                    CheeseMenuMenuID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheesesMenu", x => new { x.CheeseID, x.MenuID });
+                    table.ForeignKey(
+                        name: "FK_CheesesMenu_Cheeses_CheeseID",
+                        column: x => x.CheeseID,
+                        principalTable: "Cheeses",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CheesesMenu_Menu_MenuID",
+                        column: x => x.MenuID,
+                        principalTable: "Menu",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CheesesMenu_CheesesMenu_CheeseMenuCheeseID_CheeseMenuMenuID",
+                        columns: x => new { x.CheeseMenuCheeseID, x.CheeseMenuMenuID },
+                        principalTable: "CheesesMenu",
+                        principalColumns: new[] { "CheeseID", "MenuID" },
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cheeses_CategoryID",
+                table: "Cheeses",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CheesesMenu_MenuID",
+                table: "CheesesMenu",
+                column: "MenuID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CheesesMenu_CheeseMenuCheeseID_CheeseMenuMenuID",
+                table: "CheesesMenu",
+                columns: new[] { "CheeseMenuCheeseID", "CheeseMenuMenuID" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheesesMenu");
+
+            migrationBuilder.DropTable(
+                name: "Cheeses");
+
+            migrationBuilder.DropTable(
+                name: "Menu");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
